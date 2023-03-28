c8a26759402f7e59a8b0305580ad18b6c2f70c22
Ismaiana Cruz De Lima <ismaiana_@hotmail.com>
2023-03-27 18:09:03 -0700

    server, index.html added

---
+++ server.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template, redirect
+import os
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+
+app.jinja_env.undefined = StrictUndefined
+app.secret_key = os.environ["secret_key"]
+
+
+
+
+
+@app.route('/')
+def homepage():
+    """Display homepage."""
+
+    return render_template("index.html")
